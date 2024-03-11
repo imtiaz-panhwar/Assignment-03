f862251e1298276be1503f40f7610e96fd7f8e46
imtiaz ali <adv.imtiazpanhwar@gmail.com>
2024-03-11 16:55:40 +0500

    first commit

---
+++ app.ts
@@ -0,0 +1,13 @@
+// printing person Name in Lower case.
+
+let personName : string = "Arslan Khan";
+
+console.log("to lower case: " + personName.toLowerCase());
+
+// printing person Name in Uppercase case.
+
+console.log("to upper case: " + personName.toUpperCase());
+
+// printing person Name in titlecase case.
+
+console.log("to title case: " + personName.split('  ').map(words => words.charAt(0).toUpperCase() + personName.slice(1).toLowerCase()).join(' '));
